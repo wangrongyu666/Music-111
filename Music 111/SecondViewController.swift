fc09742241a89bdf06db1743e4338de5bee76322
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 06:46:38 +1200

    Initial Commit

---
+++ Music 111/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Music 111
+//
+//  Created by Rongyu on 27/09/19.
+//  Copyright © 2019 Rongyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
