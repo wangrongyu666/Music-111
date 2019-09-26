8b00ac21b0075ad18d9a4e105f3959c5cd53398b
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 07:55:33 +1200

    Final

--- Music 111/FirstViewController.swift
+++ Music 111/FirstViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import AVFoundation
 
-class FirstViewController: UIViewController {
+
+var audioPlayer = AVAudioPlayer()
+var songs:[String] = []
+var thisSong = 0
+var audioStuffed = false
+
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = songs[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        do{
+            let audioPath = Bundle.main.path(forResource: songs[indexPath.row], ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!)as URL)
+            audioPlayer.play()
+            thisSong = indexPath.row
+            audioStuffed = true
+        }
+        catch{
+            print ("ERROR")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        gettingSongName()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func gettingSongName() {
+        let folderURL = URL(fileURLWithPath: Bundle.main.resourcePath!)
+        
+        do {
+            let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            for song in songPath {
+                var mySong = song.absoluteString
+                
+                if mySong.contains(".mp3") {
+                    let findString = mySong.components(separatedBy: "/")
+                    mySong =  findString[findString.count-1]
+                    mySong = mySong.replacingOccurrences(of: "%20", with: " ")
+                    mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
+                    songs.append(mySong)
+                }
+            }
+            myTableView.reloadData()
+        }
+        catch {
+            
+        }
     }
 
 
 }
-
--- Music 111/SecondViewController.swift
+++ Music 111/SecondViewController.swift
@@ -1,20 +1,83 @@
-//
-//  SecondViewController.swift
-//  Music 111
-//
-//  Created by Rongyu on 27/09/19.
-//  Copyright © 2019 Rongyu Wang. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func Play(_ sender: Any)
+    {
+        if audioPlayer.isPlaying == true && audioPlayer.isPlaying == false
+        {
+            audioPlayer.play()
+        }
+    }
+    @IBAction func Pause(_ sender: Any)
+    {
+        if audioStuffed == true && audioPlayer.isPlaying
+        {
+            if audioPlayer.isPlaying
+            {
+                audioPlayer.pause()
+            }
+        }
+        
+    }
+    @IBAction func Prev(_ sender: Any)
+    {
+        if thisSong != 0 &&  audioStuffed == true
+        {
+            playThis(thisThing: songs[thisSong-1])
+            thisSong += 1
+            label.text = songs[thisSong]
+        }
+        else
+        {
+            
+        }
+    }
+    @IBAction func next(_ sender: Any)
+    {
+        if thisSong < songs.count-1 && audioStuffed == true
+        {
+        playThis(thisThing: songs[thisSong+1])
+        thisSong += 1
+        label.text = songs[thisSong]
+        }
+        else
+        {
+            
+        }
+    }
+    @IBAction func UiSlider(_ sender: Any)
+    {
+        if audioStuffed == true
+        {
+            audioPlayer.volume = (sender as AnyObject).value
+
+        }
+    }
+    func playThis(thisThing:String)
+    {
+    do{
+        let audioPath = Bundle.main.path(forResource: thisThing, ofType: ".mp3")
+        try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!)as URL)
+        
+        audioPlayer.play()
+      
+    }
+    catch{
+        print ("ERROR")
+    }
+  
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = songs[thisSong]
+     
         // Do any additional setup after loading the view.
     }
 
 
 }
-
